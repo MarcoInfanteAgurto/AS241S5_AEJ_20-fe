1254e9138eb3e2ad1f6ea9f7b1ceb589bae8752b
MarcoInfanteAgurto <marco.infante.agurto@vallegrande.edu.pe>
2026-06-01 14:33:31 -0500

    test: introduce code smells, vulnerability hotspots and vulnerable packages to trigger scan alerts

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,7 +12,19 @@ import { ImagesComponent } from './components/images/images.component';
 export class App {
   protected readonly title = signal('frontend');
   
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    // Código intencional para forzar alertas de SonarCloud
+    this.codigoVulnerableYSmells();
+  }
+  
+  // Método con code smells, secretos expuestos y lógicas redundantes para SonarCloud
+  private codigoVulnerableYSmells() {
+    var passwordInsegura = "123456_admin_secret_token_hotspot";
+    var variableNoUsada = 100;
+    if (variableNoUsada === variableNoUsada) {
+      console.log("Este bloque tiene código redundante y expone: " + passwordInsegura);
+    }
+  }
   
   setTab(tab: 'articles' | 'images') {
     this.router.navigate([`/${tab}`]);
